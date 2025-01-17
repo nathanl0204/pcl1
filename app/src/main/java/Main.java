a15397d5ba4cc3869ea77a0e5d0d83424a507f56
PLISSON Clement <clement.plisson@telecomnancy.eu>
2025-01-18 00:24:01 +0100

    modif du static de lexeur en classe normal, + modif de main

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 import AST.File;
 
@@ -8,18 +9,22 @@ public class Main {
     
     public static void main(String[] args) throws IOException {
         
-        if (args.length != 2 || (!args[0].equals("Lexer") && !args[0].equals("Parser"))){
-            System.out.println("Signature : gradle run --args=\"Lexer|Parser FilePath\"");
+        if (args.length < 2 || (!args[0].equals("Lexer") && !args[0].equals("Parser"))){
+            System.out.println("Signature : gradle run --args=\"Lexer|Parser FilePath\" option : --stack | --simplify");
         }
         else {
             //"src/main/resources/test.py"
             System.out.println("\nExecution du Lexeur\n");;
-            Lexeur.execute(args[1]);
+            Lexeur lexeur = new Lexeur();
+            lexeur.execute(args[1]);
+            if (Arrays.asList(args).contains("--stack"))
+                lexeur.print_tokens();
+
             if (args[0].equals("Parser")){
                 System.out.println("Execution du Parseur\n");
 
                 Parser parser = new Parser();
-                parser.setTokenQueueFromTokenStack(Lexeur.token_stack);
+                parser.setTokenQueueFromTokenStack(lexeur.token_stack);
                 
                 File result = parser.startAnalyse();
 
@@ -28,7 +33,8 @@ public class Main {
                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
                 bufferedWriter.write("graph AST {\n");
 
-                result.simplify();
+                if (Arrays.asList(args).contains("--simplify"))
+                    result.simplify();
                 result.vizualisation(bufferedWriter);
 
                 bufferedWriter.write("}\n");
